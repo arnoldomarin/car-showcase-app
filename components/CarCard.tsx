2ac16f30dffd74d4c78d1af7d916ea77d1aca3d6
Arnoldo Marin <arnoldo.emarin@gmail.com>
2024-01-12 11:51:29 +0100

    Finished logic and styling for details modal for each vehicle

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -82,7 +82,11 @@ const CarCard = ({ car } :  CarCardProps) => {
                 </div>
             </div>
 
-            <CarDetails />
+            <CarDetails 
+                isOpen={isOpen} 
+                closeModal={() => setIsOpen(false)}
+                car={car}
+            />
         </div>
     )
 }
