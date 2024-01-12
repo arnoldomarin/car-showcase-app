e45acab200dd11eb15f794b56a54b368fc483906
Arnoldo Marin <arnoldo.emarin@gmail.com>
2024-01-12 17:22:47 +0100

    implemented server side search. Fixed bug in next js with scroll when clicking search. Added parameters that are passed to API to retrieve wanted data based on search input

--- utils/index.ts
+++ utils/index.ts
@@ -1,14 +1,16 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
 const NEXT_PUBLIC_IMAGIN_API_KEY='hrjavascript-mastery';
 
-export async function fetchCars () {
+export async function fetchCars (filters: FilterProps) {
+    const { manufacturer, year, model, limit, fuel } = filters;
+
     const headers = {
         'X-RapidAPI-Key': '84673e5b3emsha38df24b3a9782fp10646ejsn424e942fec00',
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
 
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=m5', {
+    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
         headers: headers
     });
 
