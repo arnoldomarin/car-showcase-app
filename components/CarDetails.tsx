dee3e1c9b7fb8fb71a30bceef91741417cf5dffa
Arnoldo Marin <arnoldo.emarin@gmail.com>
2024-01-12 15:55:21 +0100

    complete modal and images for cars

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -7,6 +7,7 @@ import Image from 'next/image';
 import { Fragment } from 'react';
 
 import { Dialog, Transition } from '@headlessui/react';
+import { generateCarImageUrl } from '@/utils';
 
 interface CarDetailsProps {
     isOpen: boolean;
@@ -65,7 +66,7 @@ const CarDetails = ({ isOpen, closeModal, car} : CarDetailsProps) => {
                                 <div className='flex-1 flex flex-col gap-3'>
                                     <div className='relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg'>
                                         <Image 
-                                            src="/hero.png" 
+                                            src={generateCarImageUrl(car)}
                                             alt='Car model' 
                                             fill
                                             priority
@@ -76,7 +77,7 @@ const CarDetails = ({ isOpen, closeModal, car} : CarDetailsProps) => {
                                     <div className='flex gap-3'>
                                         <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
                                             <Image 
-                                                src="/hero.png" 
+                                                src={generateCarImageUrl(car, '29')}
                                                 alt='Car model' 
                                                 fill
                                                 priority
@@ -85,7 +86,7 @@ const CarDetails = ({ isOpen, closeModal, car} : CarDetailsProps) => {
                                         </div>
                                         <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
                                             <Image 
-                                                src="/hero.png" 
+                                                src={generateCarImageUrl(car, '33')}
                                                 alt='Car model' 
                                                 fill
                                                 priority
@@ -94,7 +95,7 @@ const CarDetails = ({ isOpen, closeModal, car} : CarDetailsProps) => {
                                         </div>
                                         <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
                                             <Image 
-                                                src="/hero.png" 
+                                                src={generateCarImageUrl(car, '13')}
                                                 alt='Car model' 
                                                 fill
                                                 priority
